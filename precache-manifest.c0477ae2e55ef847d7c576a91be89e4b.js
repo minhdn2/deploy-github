b14f90583771208b69fe74a5068b5f14c59209d9
Minh Duong <minhduongnhat@outlook.com>
2019-04-23 20:16:44 +0700

    Updates

---
+++ precache-manifest.c0477ae2e55ef847d7c576a91be89e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4a642531fcb59971f886e013f25e48",
+    "url": "/template-managmenet/index.html"
+  },
+  {
+    "revision": "c5a06d25405970ead297",
+    "url": "/template-managmenet/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/template-managmenet/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c5a06d25405970ead297",
+    "url": "/template-managmenet/static/js/main.89154ab8.chunk.js"
+  },
+  {
+    "revision": "69995198ce477e2745e1",
+    "url": "/template-managmenet/static/js/runtime~main.68d56a5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/template-managmenet/static/media/logo.5d5d9eef.svg"
+  }
+]);
